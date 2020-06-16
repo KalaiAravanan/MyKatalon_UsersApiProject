287d472fa27af68b4077f09df81398fa2593a695
AMANI <AMANI@DESKTOP-GOPJEKT>
2020-06-16 11:34:15 -0500

    my first commit - users api project

---
+++ Test Suites/UserFunctions_Testsuite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserFunctions_Testsuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>06b2725b-93d6-4a6a-a620-deb27a6a46c6</testSuiteGuid>
+   <testCaseLink>
+      <guid>30370e89-531c-425a-bbef-4a6cc7913725</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Userfunctions_Testcase</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
